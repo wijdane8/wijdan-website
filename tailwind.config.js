3844301917b53e55249b09b4afc8b92db37eb841
wijdane8 <wijdane.ali@gmail.com>
2025-04-24 10:07:43 +0300

    change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // This is crucial!
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      heading: ['Space Grotesk', 'sans-serif'],
+      mono: ['Space Mono', 'monospace']
+    },
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#1a1a1a',
+            a: {
+              color: '#2563eb',
+              '&:hover': {
+                color: '#1d4ed8',
+              },
+            },
+          },
+        },
+      }
+    }
   },
   plugins: [],
 }
